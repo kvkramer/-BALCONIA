e48fc68e8455177a66ab75ebe4a9885875642086
Katharina Kramer <katharinavk@icloud.com>
2020-06-01 15:49:17 +0200

    add

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,53 @@
+class BookingsController < ApplicationController
+
+  def index
+    #Find the user we're creating the meeting for
+    #@user = User.find(params[:user_id])
+    # Asssociate the dog and the user
+    #@dog.user = @user
+    # Display all the meetings
+
+    @meetings = Meeting.all
+
+    # @user_meetings = Meeting.where(user = current_user)
+  end
+
+  def new
+    @meeting = Meeting.new
+    set_dog
+  end
+
+  def create
+    set_dog
+    @user = current_user
+    @price = @dog.price
+    @meeting = Meeting.new(meeting_params)
+    @meeting.user_id = current_user.id
+    @meeting.dog = @dog
+    @meeting.price = @price
+    if @meeting.save
+      redirect_to meetings_path, notice: 'Added ingredient.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @meetings = Meeting.find(params[:id])
+    @meetings.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to meetings_path, notice: 'This meeting was cancelled.'
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:dog_id])
+  end
+
+  def meeting_params
+    params.require(:meeting).permit(:dates, :price, :user_id, :dog_id, :start_time, :end_time)
+  end
+
+end
---
+++ app/models/sphere.rb
@@ -0,0 +1,3 @@
+class Sphere < ApplicationRecord
+  belongs_to :owner
+end
