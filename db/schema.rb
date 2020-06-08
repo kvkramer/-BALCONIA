3af9c3f63a8fedb938f8f67e638e2561d0596961
Mealedey In <inmeallie@gmail.com>
2020-06-08 10:25:16 +0200

    Routes and Models for Chat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_122658) do
+ActiveRecord::Schema.define(version: 2020_06_08_081840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,24 @@ ActiveRecord::Schema.define(version: 2020_06_05_122658) do
     t.integer "cost_per_day"
     t.index ["sphere_id"], name: "index_bookings_on_sphere_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.bigint "sphere_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sphere_id"], name: "index_conversations_on_sphere_id"
+    t.index ["user_id"], name: "index_conversations_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -126,6 +144,10 @@ ActiveRecord::Schema.define(version: 2020_06_05_122658) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "spheres"
   add_foreign_key "bookings", "users"
+  add_foreign_key "conversations", "spheres"
+  add_foreign_key "conversations", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "bookings"
   add_foreign_key "spheres", "users"
   add_foreign_key "taggings", "tags"
